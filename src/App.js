edea2abd281699d9e6f578420e05b0dad3fb1cc5
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2025-01-18 19:26:56 -0300

    corrige ordem app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 
 function App() {
     return (
-            <div className="App">
         <Router>
-                <Header />
+            <div className="App">
+                <Header/>
                 <div className="MainContent">
-                    <Sidebar />
+                    <Sidebar/>
                     <div className="Content">
                         <Routes>
-                            <Route path="/dashboard" element={<Dashboard />} />
+                            <Route path="/dashboard" element={<Dashboard/>}/>
                         </Routes>
                     </div>
                 </div>
-        </Router>
             </div>
+        </Router>
     );
 }
 
