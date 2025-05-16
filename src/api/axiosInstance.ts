35d36ab27dc52de9738de64942f0c37d37c0083b
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2025-05-16 20:17:55 -0300

    retorno interceptor

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,42 +1,45 @@
 import axios from 'axios';
+import refreshInstance from './refreshInstance';
+import { setUserGlobally } from '../Auth/Context/AuthState';
+import { fetchUser } from '../Utils/AuthUtils';
 
 const axiosInstance = axios.create({
     baseURL: 'https://api-financial-system-production.up.railway.app',
     withCredentials: true,
 });
 
-// axiosInstance.interceptors.response.use(
-//     (response) => response,
-//     async (error) => {
-//         // const originalRequest = error.config;
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
 
-//         // if (originalRequest.url.includes('/auth/refresh')) {
-//         //     return Promise.reject(error);
-//         // }
+        if (originalRequest.url.includes('/auth/refresh')) {
+            return Promise.reject(error);
+        }
 
-//         if (
-//             error.response &&
-//             error.response.status === 401 &&
-//             !originalRequest._retry
-//         ) {
-//             originalRequest._retry = true;
+        if (
+            error.response &&
+            error.response.status === 401 &&
+            !originalRequest._retry
+        ) {
+            originalRequest._retry = true;
 
-//             try {
-//                 await refreshInstance.post('/auth/refresh');
+            try {
+                await refreshInstance.post('/auth/refresh');
 
-//                 const user = await fetchUser();
-//                 if (user) {
-//                     setUserGlobally(user); // atualiza o estado central
-//                 }
-//                 return axiosInstance(originalRequest);
-//             } catch {
-//                 window.location.href = '/login';
-//             }
-//         }
+                const user = await fetchUser();
+                if (user) {
+                    setUserGlobally(user);
+                }
+                return axiosInstance(originalRequest);
+            } catch {
+                window.location.href = '/login';
+            }
+        }
 
-//         return Promise.reject(error);
-//     }
-// );
+        return Promise.reject(error);
+    }
+);
 
 
 export default axiosInstance;
