5a2b09d31dfcdc3356e897fe25916ba031289b4d
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2025-05-16 21:36:04 -0300

    remove interceptor

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,48 +1,8 @@
 import axios from 'axios';
-import refreshInstance from './refreshInstance';
-import { fetchUser } from '../Utils/AuthUtils';
-import { setUserGlobally } from '../Auth/Context/AuthState';
 
 const axiosInstance = axios.create({
     baseURL: 'https://api-financial-system-production.up.railway.app',
     withCredentials: true,
 });
-
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
-        const currentPath = window.location.pathname;
-        const isOnLoginPage = currentPath === '/login';
-
-        if (originalRequest.url.includes('/auth/refresh')) {
-            return Promise.reject(error);
-        }
-
-        if (
-            error.response &&
-            error.response.status === 401 &&
-            !originalRequest._retry
-            && !isOnLoginPage
-        ) {
-            originalRequest._retry = true;
-
-            try {
-                await refreshInstance.post('/auth/refresh');
-
-                const user = await fetchUser();
-                if (user) {
-                    setUserGlobally(user); // atualiza o estado central
-                }
-                return axiosInstance(originalRequest);
-            } catch {
-                window.location.href = '/login';
-            }
-        }
-
-        return Promise.reject(error);
-    }
-);
-
 
 export default axiosInstance;
