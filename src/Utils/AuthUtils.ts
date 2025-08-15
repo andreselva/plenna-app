1c3cc63527c79bf66fada80bde2ddc2e8dae8c03
André Selva <selvaandre99@gmail.com>
2025-08-15 16:44:18 -0300

    ajustes

--- src/Utils/AuthUtils.ts
+++ src/Utils/AuthUtils.ts
@@ -3,7 +3,7 @@ import axiosInstance from "../api/axiosInstance";
 export async function fetchUser(): Promise<any | null> {
     try {
         const res = await axiosInstance.get('/auth');
-        const user = res.data.user;  // direto aqui
+        const user = res.data.user;
         return user || null;
     } catch {
         return null;
